2ee6237f0753baf076855e190e25e29abea63ef8
Calmes <christophe.calmes@laplateforme.io>
2023-12-08 16:02:43 +0100

    figth club

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
 const Fighter = require('./src/Fighter')
- const test = new Fighter('Chris', '5', '4');
- console.log(test);
+const debug = true;
+const figth_club = true;
+
+const Heracles = new Fighter('🧔 Heracles', 20, 6);
+const Nemean = new Fighter('🦁 Lion', 11, 13);
+
+if(debug) {
+    console.info(Heracles);
+    console.info(Nemean);
+    console.log(Heracles.setLife())
+    console.log(Nemean.setLife());
+}
+if(figth_club) {
+    for (let index = 0; index < 4; index++) {
+        Heracles.figth(Nemean);
+        Nemean.figth(Heracles);
+        console.log(Heracles.setLife())
+        console.log(Nemean.setLife());
+    }
+}
--- src/Fighter.js
+++ src/Fighter.js
@@ -12,7 +12,21 @@ class Fighter {
         this.dexterity = dexterity;
         this.life = MAX_LIFE;
     }
+    setLife() {
+        return `This ${this.name} have ${this.life} point of life.`;
+    }
+    figth(figther) {
+       const  dammagePoint = Math.floor(Math.random() * this.strength);
+        const dodge = Math.floor(Math.random() * figther.dexterity);
+        const lostPointOfLife = dammagePoint - dodge;
+        if (lostPointOfLife>0) {
+            this.life = this.life - (dammagePoint - dodge);
+            return `${this.name} hit ${figther.name} attack = ${lostPointOfLife} !`;
+        } else {
+            return `${this.name} miss ${figther.name} !?`;
+        }
+    }
 
 }
 
-module.exports = Fighter
+module.exports = Fighter;
